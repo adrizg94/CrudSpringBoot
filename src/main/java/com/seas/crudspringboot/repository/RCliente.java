02447ebdf163ed7b6714a6299d8a1f62a5edb9f7
adrizg94 <adrizg_94@hotmail.com>
2024-01-02 18:34:44 +0100

    Test ssh

--- src/main/java/com/seas/crudspringboot/repository/RCliente.java
+++ src/main/java/com/seas/crudspringboot/repository/RCliente.java
@@ -4,5 +4,4 @@ import com.seas.crudspringboot.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RCliente extends JpaRepository<Cliente, Long> {
-    
 }
